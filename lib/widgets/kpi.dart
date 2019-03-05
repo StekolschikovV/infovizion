e8161022b965b5fa71b883e3dd62cb8c19bba1bc
v.a.stekolschikov <v.a.stekolschikov@gmail.com>
2019-03-05 10:30:11 +0200

    Kpi add param

--- lib/widgets/kpi.dart
+++ lib/widgets/kpi.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Kpi {
-  num width = 30;
-  num height = 30;
 
-  Kpi(BuildContext context){
-    print('KPI');
+  double width = 30;
+  double height;
+
+  int colorRed;
+  int colorGreen;
+  int colorBlue;
+  double colorOpacity;
+
+  Kpi(BuildContext context,
+      {this.colorRed: 63, this.colorGreen: 191, this.colorBlue: 63, this.colorOpacity: 1, this.height = 150}) {
+    width = MediaQuery.of(context).size.width;
   }
 
   Widget get() {
     return Container(
       margin: const EdgeInsets.all(10.0),
-      color: const Color(0xFF00FF00),
+//      color: const Color('0xFF00FF00'),
+      color: Color.fromRGBO(colorRed, colorGreen, colorBlue, colorOpacity),
 //      width: MediaQuery.of(context).size.width,
-      width: 30,
-      height: 48.0,
+      width: width,
+      height: height,
     );
-
   }
 }
