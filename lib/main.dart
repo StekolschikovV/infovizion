b98d8c1fdb1334b9342bf3e923592c8ea9ff84ec
v.a.stekolschikov <v.a.stekolschikov@gmail.com>
2019-03-06 13:15:35 +0200

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import './pages/page2.dart';
 import './pages/page1.dart';
+import './pages/test.dart';
 
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'infovizion',
-      initialRoute: '/',
+      initialRoute: '/page2',
       routes: {
         '/': (context) => Page1(title: 'page 1'),
-        '/page2': (context) => Page2(title: 'page 2'),
+        '/page2': (context) => Page2(title: 'page 2',),
+//        '/page2': (context) => DateTimeComboLinePointChart(),
       },
     );
   }
--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
+import './test.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:infovizion/bar_chart/simple.dart';
 
-class Page2 extends StatefulWidget{
+class Page2 extends StatefulWidget {
   Page2({Key key, this.title}) : super(key: key);
   final String title;
+
   @override
   _Page2 createState() => _Page2();
 }
@@ -14,7 +19,13 @@ class _Page2 extends State<Page2> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: new Text('Page2')
-    );
+//        body:  new SimpleBarChart.withRandomData(),
+        body: Column(
+          children: <Widget>[
+          ],
+        )
+//        body: HorizontalPatternForwardHatchBarChart()
+        );
   }
 }
+
