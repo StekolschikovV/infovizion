8031f137e3bab922cd261cde5b7eddf27bbf6eeb
v.a.stekolschikov <v.a.stekolschikov@gmail.com>
2019-03-06 13:31:55 +0200

    temp

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -3,6 +3,8 @@ import 'package:charts_flutter/flutter.dart';
 import './test.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:infovizion/bar_chart/simple.dart';
+import 'package:infovizion/bar_chart/stacked_horizontal.dart';
+//import 'package:infovizion/bar_chart/d';
 
 class Page2 extends StatefulWidget {
   Page2({Key key, this.title}) : super(key: key);
@@ -20,10 +22,34 @@ class _Page2 extends State<Page2> {
           title: Text(widget.title),
         ),
 //        body:  new SimpleBarChart.withRandomData(),
-        body: Column(
+        body:  ListView(
           children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                    width: 200.0,
+                    height: 200.0,
+                    child: SimpleBarChart.withSampleData()
+                ),
+                Container(
+                  height: 200.0,
+                  width: 200.0,
+                  child: StackedHorizontalBarChart.withSampleData(),
+                ),
+//                Container(
+//                  height: 200.0,
+//                  width: 200.0,
+//                  child: new DonutPieChart.withRandomData(),
+//                ),
+              ],
+            ),
           ],
         )
+//        body: Column(
+//          children: <Widget>[
+//            MyHomePage2222()
+//          ],
+//        )
 //        body: HorizontalPatternForwardHatchBarChart()
         );
   }
